e8a188f93ef2bf48fa03bbbe0f85b759a3696299
Simone Palmisciano <simonepalmisciano11@gmail.com>
2026-04-17 12:45:30 +0200

    creata la fetch con l'API

--- scripts/script.js
+++ scripts/script.js
@@ -3,3 +3,19 @@
 
 const API_URL = 'https://lanciweb.github.io/demo/api/pictures/';
 
+const cardContainerElem = document.querySelector('#cardContainer');
+
+if (cardContainerElem !== null) {
+    fetch(API_URL)
+        .then(response => {
+            console.log(response);
+            return response.json();
+        })
+        .then(jsonData => {
+            console.log(jsonData);
+        })
+        .catch(error => {
+            console.error(error);
+            
+        });
+}
